a5014910da9e0103d2505b9dcb856e64aceab72e
Moritz Petersen <moritz.petersen@freenet.de>
2020-03-26 12:09:09 +0100

    Bind fwrite

--- src/io.lisp
+++ src/io.lisp
@@ -6,7 +6,13 @@
                            :one-liner t)))
     (not (= 0 eof))))
 
+;; fread+fwrite: add length parameter, keep full vector as default
 (defun fread (vec file-pointer)
   (ffi:c-inline (vec file-pointer) (:object :pointer-void) :int
                 "fread(#0->vector.self.b8, #0->vector.fillp, 1, #1)"
+                :one-liner t))
+
+(defun fwrite (vec file-pointer)
+  (ffi:c-inline (vec file-pointer) (:object :pointer-void) :int
+                "fwrite(#0->vector.self.b8, #0->vector.fillp, 1, #1)"
                 :one-liner t))
